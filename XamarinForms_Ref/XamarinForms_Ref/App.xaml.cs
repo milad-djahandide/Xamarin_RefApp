5a4979cb556f11fae410a5f8d869418dd42bc8bb
milad <milad@DESKTOP-4DQQ9LN>
2020-03-25 19:31:11 +0430

    First Solution Added

---
+++ XamarinForms_Ref/XamarinForms_Ref/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinForms_Ref.Services;
+using XamarinForms_Ref.Views;
+
+namespace XamarinForms_Ref
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinForms_Ref/XamarinForms_Ref/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinForms_Ref.Models;
+
+namespace XamarinForms_Ref.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
