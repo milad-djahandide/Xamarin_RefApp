e1da5066a1a03a7f1d9ec2906854177f5b452f74
milad <milad@DESKTOP-4DQQ9LN>
2020-03-31 08:49:04 +0430

    MoshHamedani Examples Solution Added

---
+++ MoshHamedaniExamples/MoshHamedaniExamples/Xaml_Examples/Component_Binding.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoshHamedaniExamples.Xaml_Examples
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Component_Binding : ContentPage
+    {
+        public Component_Binding()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
