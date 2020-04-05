02d6b99839ba63ebb284c16201c8f6b62d55e442
milad <milad@DESKTOP-4DQQ9LN>
2020-04-06 01:08:32 +0430

    Extra Actions Of List Added

---
+++ MoshHamedaniExamples/MoshHamedaniExamples/Lists/List_ExtraActions.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoshHamedaniExamples.Lists
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class List_ExtraActions : ContentPage
+    {
+        public List_ExtraActions()
+        {
+            InitializeComponent();
+        }
+    }
+}
