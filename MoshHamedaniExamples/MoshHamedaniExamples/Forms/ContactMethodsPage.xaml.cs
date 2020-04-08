b365b138fd6793bedb835bff11e1b9273c71c95f
milad <milad@DESKTOP-4DQQ9LN>
2020-04-08 12:47:27 +0430

    another part of Forms Added

---
+++ MoshHamedaniExamples/MoshHamedaniExamples/Forms/ContactMethodsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoshHamedaniExamples.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactMethodsPage : ContentPage
+    {
+        public ContactMethodsPage()
+        {
+            InitializeComponent();
+            listView.ItemsSource = new List<string>
+            {
+                "None",
+                "Email",
+                "Call",
+                "SMS"
+            };
+        }
+        public ListView ContactMethods { get { return listView; } }
+    }
+}
---
+++ MoshHamedaniExamples/MoshHamedaniExamples/Forms/PickerWithNavigation.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoshHamedaniExamples.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerWithNavigation : ContentPage
+    {
+        public PickerWithNavigation()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewCell_Tapped(object sender, EventArgs e)
+        {
+            var page = new ContactMethodsPage();
+            page.ContactMethods.ItemSelected += (source, args) =>
+            {
+                contactMethod.Text = args.SelectedItem.ToString();
+                Navigation.PopAsync();
+            };
+            Navigation.PushAsync(page);
+        }
+    }
+}
